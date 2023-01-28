cbc249c3914082ae40594bc558e769444ad71862
kate <kate@pop-os.localdomain>
2023-01-29 07:09:50 +0900

    disable i18n

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,9 @@ const config = {
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  /* i18n: {
+   *   locales: ["en"],
+   *   defaultLocale: "en",
+   * }, */
 };
 export default config;
